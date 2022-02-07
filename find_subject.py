ec5f7cc64aed007ec82d6761b35c7b14e67cebfa
Hugo BRETECHE <hubretec@e2r6p15.clusters.42paris.fr>
2022-02-07 16:25:58 +0100

    subject done

--- find_subject.py
+++ find_subject.py
@@ -20,4 +20,42 @@ def clear_str(txt):
 	txt = sub(r"\-", "", txt)
 	return (txt)
 
-print(clear_str("#cool I love my @Tesla \n    Model Y more é with every update"))
+def get_subject(txt) -> list():
+	nlp = spacy.load("en_core_web_lg")
+	doc = nlp(txt)
+	subjs = []
+	for ent in doc.ents:
+		subjs.append(ent.text)
+	return (subjs)
+
+def	hugo(txt :str, dico: dict) -> dict:
+	
+	doc = nlp(txt)
+	for topic in doc.ents:
+		if topic.text not in dico.keys():
+			dico[topic.text] = [txt]
+		else:
+			if (txt not in dico[topic.text]):
+				dico[topic.text].append(txt)
+	return (dico)
+
+
+if (__name__ == "__main__"):
+	dico = {}
+	nlp = spacy.load("en_core_web_lg")
+	txt = "I'm Joe Biden. Jules is an enormous idiot. Bob. Apple is an amazing compagny. Jules."
+	hugo(txt, dico)
+	txt = "I'm Spiderman. Jules is an enormous idiot. Bob. Apple is an amazing compagny. Jules."
+	hugo(txt, dico)
+	# nlp = spacy.load("en_core_web_lg")
+	
+	# doc = nlp(txt)
+	# text_topics = []
+	# # [[topic, [strings]], [topic2, [str2]]
+	# ret = []
+	# for topic in doc.ents:
+	# 	if (topic.text not in text_topics):
+	# 		top = [topic.text, txt]
+	# 		ret.append(top)
+	# 		text_topics.append(topic.text)
+	print(dico)
